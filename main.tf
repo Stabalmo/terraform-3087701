1fc3beee57b036bcd034afa1718888e0e9c68548
Danila Alferov <danila.alferov.dev@gmail.com>
2026-01-11 19:06:26 +0000

    Refactor formatting and improve readability in main.tf and providers.tf

--- main.tf
+++ main.tf
@@ -24,18 +24,18 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   subnet_id = module.blog_vpc.public_subnets[0]
@@ -57,20 +57,23 @@ module "alb" {
 
 
   listeners = {
-    http_tcp_listeners = {
+    http = {
       port     = 80
       protocol = "HTTP"
-      target_group_index = 0
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
     }
   }
 
   target_groups = {
     ex-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      name_prefix = "blog-"
+      protocol    = "HTTP"
+      port        = 80
+      target_type = "instance"
+      target_id   = aws_instance.blog.id
     }
   }
 
