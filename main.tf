04e67627db25632a8ffb82a94341f4d1d5f0fea0
Danila Alferov <danila.alferov.dev@gmail.com>
2026-01-11 15:57:30 +0000

    fix 4

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow HTTP and HTTPS in. Alllow everithing out."
 
-  vpc_id = data.aws_vps.default.id
+  vpc_id = data.aws_vpc.default.id
 
 }
 
