5b7639e1f1d1470da19029cf3dcc17dc14234074
Danila Alferov <danila.alferov.dev@gmail.com>
2026-01-11 18:40:21 +0000

    load balancer

--- main.tf
+++ main.tf
@@ -45,6 +45,40 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+
+  # Security Group
+  security_groups = module.blog_vpc.security_group_id
+
+
+  listeners = {
+    http_tcp_listeners = {
+      port     = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.1"
